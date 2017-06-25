fbc2d7f9523b0496da3207a843b82e27c3bce832
NickChuCode <nickchu15@outlook.com>
2017-06-26 01:11:50 +0800

    Revealing Module Pattern added

--- Module/taskRepository.js
+++ Module/taskRepository.js
@@ -1,16 +1,21 @@
-var repo = function(){
+var repo = function () {
+
+    var db = {};
+
+    var get = function (id) {
+        console.log('Getting task' + id);
+        return {
+            name: 'new task from db'
+        }
+    }
+
+    var save = function (task) {
+        console.log('saving ' + task.name + ' to the db');
+    }
 
     return {
-        get: function(id){
-            console.log('Getting task' + id);
-            return{
-                name: 'new task from db'
-            }
-        },
-
-        save: function(task){
-            console.log('saving ' + task.name + ' to the db');
-        }
+        get: get,
+        save: save
     }
 }
 
