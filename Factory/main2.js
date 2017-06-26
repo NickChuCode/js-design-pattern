d9700d6413ddb687c0f0c7dcb30f07bf9844881e
NickChuCode <nickchu15@outlook.com>
2017-06-27 02:12:28 +0800

    factory with cache added

--- Factory/main2.js
+++ Factory/main2.js
@@ -1,5 +1,5 @@
 var Task = require('./task');
-var repoFactory = require('./repoFactory');
+var repoFactory = require('./repoFactorywCache');
 
 var task1 = new Task(repoFactory.getRepo('task').get(1));
 var task2 = new Task(repoFactory.getRepo('task').get(2));
---
+++ Factory/repoFactorywCache.js
@@ -0,0 +1,25 @@
+var repoFactory = function () {
+    this.getRepo = function (repoType) {
+        if (repoType === 'task') {
+            if (this.taskRepo) {
+                console.log('Retrieving from cache');
+                return this.taskRepo;
+            } else {
+                this.taskRepo = require('./taskRepository')();
+                //这里放一些config的代码
+                return this.taskRepo;
+            }
+
+        }
+        if (repoType === 'user') {
+            var userRepo = require('./userRepository')();
+            return userRepo;
+        }
+        if (repoType === 'proj') {
+            var projRepo = require('./projRepository')();
+            return projRepo;
+        }
+    }
+};
+
+module.exports = new repoFactory; //话说，为什么new的时候不在后面加括号呢？
